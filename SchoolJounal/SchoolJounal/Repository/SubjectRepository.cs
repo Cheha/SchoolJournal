4fcc288d5d9e4b40100c1d3f6d6bb2504ee94c64
Alexandr SHad <shadurskij@gmail.com>
2017-12-01 23:02:41 +0200

    add SubjectRepository

---
+++ SchoolJounal/SchoolJounal/Repository/SubjectRepository.cs
@@ -0,0 +1,32 @@
+﻿using SchoolJounal.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolJounal.Model;
+using SchoolJounal.Data;
+
+namespace SchoolJounal.Repository
+{
+    public class SubjectRepository : ISubjectRepository
+    {
+        private readonly AppContext _context;
+
+        public SubjectRepository()
+        {
+            _context = new AppContext();
+        }
+
+        public void Create(Subject subject)
+        {
+            _context.Subjects.Add(subject);
+            _context.SaveChanges();
+        }
+
+        public List<Subject> GetAllSubjects()
+        {
+            return _context.Subjects.ToList();
+        }
+    }
+}
