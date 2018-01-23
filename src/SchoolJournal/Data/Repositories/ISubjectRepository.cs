28b3caf337ea3e10449dbdba50566fa0341453c1
smeyanov <smeyanovr@gmail.com>
2018-01-23 21:54:27 +0200

    Создал  репозиторий SubjectRepository и интерфейс ISubjectRepository

---
+++ src/SchoolJournal/Data/Repositories/ISubjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolJournal.Data.Repositories
+{
+    public interface ISubjectRepository
+    {
+        void Add(Subject subject);
+        List<Subject> Get();
+        Subject Get(int id);
+
+    }
+}
---
+++ src/SchoolJournal/Data/Repositories/Subjectrepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.Data.Repositories
+{
+    public class SubjectRepository : ISubjectRepository
+    {
+        private readonly AppDbContext context;
+
+        public SubjectRepository()
+        {
+            _context = new AppDbContext();
+        }
+        public void Add(Subject subject)
+        {
+            _context.Add.Subject(subject);
+            _context.SaveChanges();
+        }
+        public List<Subject> Get()
+        {
+            return _context.Subject
+                .ToList();
+        }
+        public Subject Get(int id)
+        {
+            return _context.Subject
+                .FirstOrDefault(_ => _.Id == id);
+        }
+    }
+}
