f1b50e5d7a8382b8930d477c8217fafa2e29f127
Ihor <33543509+IhorGH@users.noreply.github.com>
2018-01-24 13:28:27 +0200

    Данный образ проекта приведён полностью к проекту в репозитории https://github.com/Cheha/SchoolJournal (на 13.00  24-01-2018) и в него добавленны моё задание (сущ. Teacher - классы Сервис\Репозиторий\Контроллер).

---
+++ src/SchoolJournal/Data/Repository/ISchoolClassRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolJournal.Domain;
+
+namespace SchoolJournal.Data.Repository
+{
+    interface ISchoolClassRepository
+    {
+        void AddSchoolClass(SchoolClass schoolClass);
+        List<SchoolClass> GetSchoolClassList();
+        List<Student> GetStudentsList(int schoolClassId);
+        List<Subject> GetSubjectsList(int schoolClassId);
+    }
+}
---
+++ src/SchoolJournal/Services/ISchoolClassService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolJournal.Models;
+
+namespace SchoolJournal.Services
+{
+    interface ISchoolClassService
+    {
+        List<SchoolClassViewModel> _getAllSchoolClass();
+
+    }
+}
