1625c224b77f1049be7482fe894ee0612b82668d
smeyanov <smeyanovr@gmail.com>
2018-01-25 19:51:23 +0200

    Добавил Service,View,Binding

---
+++ src/SchoolJournal/Controllers/SubjectController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private readonly ISubjectService subjectService;
+
+        public SubjectController()
+        {
+            _subjectService = new SubjectService();
+        }
+
+        public ActionResult Index()
+        {
+            var subjects = _subjectService.GetAllSubjects();
+            return View(subjects);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(SubjectBindingModel subject)
+        {
+            _subjectService.AddSubject(subject);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string subjectNumber)
+        {
+            var subjectViewModel = _subjectSrevice.GetSubject(subjectNumber);
+            return View(subjectViewModel);
+        }
+    }
+}
---
+++ src/SchoolJournal/Servises/SubjectService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.Servises
+{
+    public class SubjectService : ISubjectService
+    {
+        private readonly ISubjectRepository subjectRepository;
+        private readonly IHashidsService hashidsService;
+
+        public SubjectService()
+        {
+            _subjectRepository = new SubjectRepository();
+            _hashidsService = new HashidsService();
+        }
+
+        public void AddSubject(SubjectBindinModel model)
+        {
+            var subject = new Subject
+            {
+                
+            };
+
+            _subjectRepository.Add(subject);
+        }
+
+        public List<SubjectViewModel> GetAllSubjects()
+        {
+            var subject = _subjectRepository.Get();
+            return subject.Select(_ => new SubjectViewModel
+            {
+                SubjectNumber = _hashidsSesrvice.Encode(_.Id)
+                
+            }).ToList();
+        }
+        public SubjectViewModel GetSubject(string subjectNumber)
+        {
+            var subjectId = _hashidsService.Decode(subjectNumber);
+            var subject = _subjectRepository.Get(subjectId);
+            return new SubjectViewModel
+            {
+                
+                SubjectNumber = subjectNumber
+
+            };
+        }
+    }
+}
