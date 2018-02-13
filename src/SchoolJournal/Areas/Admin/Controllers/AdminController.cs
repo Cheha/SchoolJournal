31a9fff524b413d3c2a9bbca43b69ff318f0a1ec
Ihor <33543509+IhorGH@users.noreply.github.com>
2018-02-13 21:11:34 +0200

    1) Создал область Админ
    2) Добавил [Autorize(Role="admin")]
    
    3) Shared Layouts :
    3.1) _LoginPartial => Убранна ссылка "Register" ;
    3.2) _Layout => убранны ссылки "Home","Contact","About" переименованны и перенесенны в _LoginPartial ;

---
+++ src/SchoolJournal/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+using System.Security.Authentication;
+
+namespace SchoolJournal.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
