5100a70dfed3ba88638503b1c152c2abf8214f49
Alexandr SHad <shadurskij@gmail.com>
2018-03-13 06:44:55 +0200

    add date to Marks table

--- src/SchoolJournal/Domain/Mark.cs
+++ src/SchoolJournal/Domain/Mark.cs
@@ -9,6 +9,7 @@ namespace SchoolJournal.Domain
     {
         public int Id { get; set; }
         public int Value { get; set; }
+        public DateTime Date { get; set; }
 
         public int StudentId { get; set; }
         public Student Student { get; set; }
