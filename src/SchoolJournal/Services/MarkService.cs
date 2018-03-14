e37de983ff41eccd5ecb64bac75b9ae904c2e12f
Alexandr SHad <shadurskij@gmail.com>
2018-03-14 19:17:20 +0200

    added repository, service and viewmodel for Mark

---
+++ src/SchoolJournal/Services/MarkService.cs
@@ -0,0 +1,37 @@
+﻿using SchoolJournal.Data.Repositories;
+using SchoolJournal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.Services
+{
+    public class MarkService
+    {
+        private readonly MarkRepository _markRepository;
+        private readonly HashidService _hashidService;
+
+        public MarkService()
+        {
+            _markRepository = new MarkRepository();
+            _hashidService = new HashidService();
+        }
+
+        public List<MarkViewModel> GetMarks(DateTime dateFrom, DateTime dateTill, string schoolClassId, string subjectId)
+        {
+            return _markRepository.GetMarks(
+                dateFrom,
+                dateTill,
+                _hashidService.Decode(schoolClassId),
+                _hashidService.Decode(subjectId)
+            )
+            .Select(m => new MarkViewModel {
+                Date = m.Date,
+                StudentId = _hashidService.Encode(m.StudentId),
+                SubjectId = _hashidService.Encode(m.SubjectId),
+                Value = m.Value.ToString()
+            }).ToList();
+        }
+    }
+}
