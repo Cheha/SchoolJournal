e1a22519d01dfe3bc49d3573d4b73e719c81a33c
Alexandr SHad <shadurskij@gmail.com>
2018-03-14 19:21:42 +0200

    added GetStudents, GetSubjectsList methods to SchoolClassService. added view model for Student.

---
+++ src/SchoolJournal/Models/StudentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.Models
+{
+    public class StudentViewModel
+    {
+        public string StudentId { get; set; }
+
+        [Display(Name = "First name")]
+        public string StudentFirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        public string StudentLastName { get; set; }
+
+        [Display(Name = "Full name")]
+        public string StudentFullName
+        {
+            get
+            {
+                return String.Format("{0} {1}", StudentLastName, StudentFirstName);
+            }
+        }
+
+        public string SchoolClassId { get; set; }
+    }
+}
