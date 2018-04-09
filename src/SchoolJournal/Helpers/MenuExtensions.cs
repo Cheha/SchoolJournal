34ae81e86936bdb401e37f833e6e4221d5e3ce3a
Alexandr SHad <shadurskij@gmail.com>
2018-04-10 01:20:42 +0300

    implemented changing active class for sidebar

---
+++ src/SchoolJournal/Helpers/MenuExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolJournal.Helpers
+{
+    public static class MenuExtensions
+    {
+        public static bool IsActive(this HtmlHelper html, string controller)
+        {
+            var routeValues = html.ViewContext.RouteData.Values;
+            var currentController = routeValues["controller"].ToString();
+
+            return controller == currentController;
+        }
+    }
+}
