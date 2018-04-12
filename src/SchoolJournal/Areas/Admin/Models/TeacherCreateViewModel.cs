ab77eb13cfeb1fda7ee9b2f663d90adffcabf68f
Alexandr SHad <shadurskij@gmail.com>
2018-04-13 00:49:46 +0300

    working at teacher create view

--- src/SchoolJournal/Areas/Admin/Models/TeacherCreateViewModel.cs
+++ src/SchoolJournal/Areas/Admin/Models/TeacherCreateViewModel.cs
@@ -9,20 +9,31 @@ namespace SchoolJournal.Areas.Admin.Models
     public class TeacherCreateViewModel
     {
         [Required]
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+
         [Required]
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
         [Required]
+        [Display(Name = "Отчество")]
         public string FatherName { get; set; }
+
         [Required]
         [EmailAddress]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
+
         [Required]
         [DataType(DataType.Password)]
         [Compare("Password")]
+        [Display(Name = "Подтверждение")]
         public string PasswordConfirm { get; set; }
     }
 }
